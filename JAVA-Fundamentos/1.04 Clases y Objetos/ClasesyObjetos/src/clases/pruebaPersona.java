8e4a7f4e47912096f4a67af987db46b3aef1748d
RaulCM-27 <136116606+RaulCM-27@users.noreply.github.com>
2024-04-20 11:02:36 -0500

    Add files via upload

---
+++ JAVA-Fundamentos/1.04 Clases y Objetos/ClasesyObjetos/src/clases/pruebaPersona.java
@@ -0,0 +1,18 @@
+
+package clases;
+
+public class pruebaPersona {
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.nombre = "Juan";
+        persona1.apellido = "perez";
+        persona1.despegarInformacion();
+        
+        Persona persona2 = new Persona();
+        persona2.nombre = "Karla";
+        persona2.apellido = "Lara";
+        persona2.despegarInformacion();
+    }
+    
+}
