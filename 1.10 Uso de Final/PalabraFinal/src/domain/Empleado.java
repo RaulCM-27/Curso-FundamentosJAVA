eb57c4fdea12efd2b584b02fa7826a5e2169839d
RaulCM-27 <136116606+RaulCM-27@users.noreply.github.com>
2024-04-20 11:09:22 -0500

    Add files via upload

---
+++ 1.10 Uso de Final/PalabraFinal/src/domain/Empleado.java
@@ -0,0 +1,12 @@
+
+package domain;
+
+//La clase padre final (final Persona) evita que se cree la subclase (Empleado)
+public class Empleado //extends Persona 
+{
+    
+//    @Override ---> es opcional
+//    public void imprimir(){
+//        System.out.println("Metodo imprimir desde clase hija");
+//    }
+}
